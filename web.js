cbd1dc08d7d405be4e2c92c5fb0d20d5569ce0d9
Ubuntu <ubuntu@ip-172-31-21-242.us-west-2.compute.internal>
2013-07-13 03:41:21 +0000

    alternado web.js

--- web.js
+++ web.js
@@ -2,9 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var index =  new Buffer(fs.readFile('index.html'),'utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('Hello World 2X');
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
